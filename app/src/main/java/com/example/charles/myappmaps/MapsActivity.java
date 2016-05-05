faa8f7c29f7d7679cd63b06420acf31b65521211
Charles de Souza Ferreira <charles@Charless-MacBook-Pro.local>
2016-05-05 18:41:10 -0300

    Initial commit

---
+++ app/src/main/java/com/example/charles/myappmaps/MapsActivity.java
@@ -0,0 +1,54 @@
+package com.example.charles.myappmaps;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        // Add a marker in Sydney and move the camera
+        LatLng charlesAdress = new LatLng(-12.943973, -38.486173);
+        mMap.addMarker(new MarkerOptions().position(charlesAdress).title("House of Charles"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(charlesAdress));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(charlesAdress, 10));
+
+
+        LatLng ufbaIM = new LatLng(-13.001421, -38.507473);
+        mMap.addMarker(new MarkerOptions().position(ufbaIM).title("IM-UFBA - Ondina"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ufbaIM));
+    }
+}
